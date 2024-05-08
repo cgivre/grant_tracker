a33c6263f152405cb38c8952d53c058ba36e57bf
Charles Givre <charlesgivre@Charless-Mac-Studio.local>
2024-05-07 23:54:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import datetime
+import pandas as pd
+
+import streamlit as st
+
+try:
+    grants = pd.read_csv('grants.csv')
+except:
+    grants = pd.DataFrame()
+
+def record_grant(grant_name: str, grant_description: str,
+                 grant_duration: str, grant_amount: float, grant_categories: list) -> pd.DataFrame:
+    global grants
+
+    # TODO Validate Data
+
+    # Create a dictionary
+    grant = {
+        "grant_name": grant_name,
+        "grant_description": grant_description,
+        "grant_start_date": grant_duration[0],
+        "grant_end_date": grant_duration[1],
+        "grant_amount": grant_amount,
+        "grant_categories": str(grant_categories)
+    }
+    temp_df = pd.DataFrame([grant])
+    grants = pd.concat([grants, temp_df])
+    grants.to_csv('grants.csv', index=False)
+    return grants
+
+
+@st.experimental_dialog("Record a Grant")
+def create_grant():
+    st.header("Enter a Grant")
+    with st.form(key="create_grant", clear_on_submit=True):
+        grant_name = st.text_input(label="Grant Name",
+                                   placeholder="Enter the Grant Name",
+                                   help="Enter a Grant Name")
+        grant_description = st.text_area(label="Grant Description",
+                                         placeholder="Enter a brief description of the grant.",
+                                         help="Enter a brief description of the grant.",
+                                         )
+
+        grant_duration = st.date_input(label="Grant Duration",
+                                       help="Enter the start and end dates for your grant.",
+                                       value=[])
+        grant_amount = st.number_input(label="Grant Amount",
+                                       help="Enter the amount of the grant.",
+                                       placeholder="Enter the amount of the grant.",
+                                       min_value=0,
+                                       step=100)
+        grant_categories = st.multiselect(label="Grant Categories",
+                                          help="Enter the categories of your grant.",
+                                          options=['Early Childhood', 'Education',
+                                                   'General Purpose', 'Food', 'Security',
+                                                   'Transportation'])
+        submitted = st.form_submit_button("Submit Grant")
+        if submitted:
+            global grants
+            grants = record_grant(grant_name, grant_description, grant_duration, grant_amount, grant_categories)
+            st.rerun()
+
+
+if st.button("Add Grant"):
+    create_grant()
+
+if len(grants) > 0:
+    st.data_editor(data=grants, use_container_width=True, hide_index=True, num_rows="dynamic")
